51e44fb0a5f9927e97052e93b890556e2978a220
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-05-10 18:56:47 -0400

    removed unneeded SQL

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* jshint esversion: 6 */
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db');
 const fs = require('fs');
 
 const app = express();
