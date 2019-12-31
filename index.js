cc01a0c34d315c61d820233e0e96c1b8640810be
vandesm14 <vandesm14@gmail.com>
2019-12-31 00:16:43 -0500

    clean up a bit

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use('/p', express.static('public'));
